25711cd3e7728b4e6b2cacd69d55174ee708f8b7
focusotter <mtliendo@focusotter.com>
2023-10-09 12:09:17 -0500

    final deploy

--- backend/lib/hosting/amplify.ts
+++ backend/lib/hosting/amplify.ts
@@ -1,6 +1,7 @@
 import { SecretValue } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import * as amplify from '@aws-cdk/aws-amplify-alpha'
+import { BuildSpec } from 'aws-cdk-lib/aws-codebuild'
 
 type AmplifyHostingProps = {
 	appName: string
@@ -16,7 +17,6 @@ export function createAmplifyHosting(
 ) {
 	const amplifyApp = new amplify.App(scope, `${props.appName}-hosting`, {
 		appName: `${props.appName}`,
-
 		sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
 			owner: props.ghOwner,
 			repository: props.repo,
@@ -33,6 +33,26 @@ export function createAmplifyHosting(
 		environmentVariables: {
 			myAmplifyEnv: 'frontend', //process.env.myAmplifyEnv
 		},
+		buildSpec: BuildSpec.fromObjectToYaml({
+			version: 1,
+			frontend: {
+				phases: {
+					preBuild: {
+						commands: ['npm ci'],
+					},
+					build: {
+						commands: ['npm run build'],
+					},
+				},
+				artifacts: {
+					baseDirectory: '.next',
+					files: ['**/*'],
+				},
+				cache: {
+					paths: ['node_modules/**/*'],
+				},
+			},
+		}),
 	})
 
 	amplifyApp.addBranch(props.branch, {
